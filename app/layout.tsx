a00351e4de54cff2821469a93627700d97e7f1a4
DrawingNea <lcy.li@outlook.de>
2023-12-31 12:08:22 +0100

    Add Data and install react icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,25 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Evelyn | Personal Portfolio',
-  description: 'Evelyn is an experienced full-stack developer',
-}
+  title: "Evelyn | Personal Portfolio",
+  description: "Evelyn is an experienced full-stack developer",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative `}>
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] "></div>
+        <div className="bg-[#dbd7fb] absolute top-[-6rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
---
+++ lib/data.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Computer Science Expert Subject Area: Software Development",
+    location: "Berlin, DE",
+    description:
+      "Over 3 years I got skilled in crafting robust and scalable software solutions, adept at coding, debugging, and optimizing applications for diverse platforms.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2020 - 2023",
+  },
+  {
+    title: "Software Developer",
+    location: "Berlin, DE",
+    description:
+      "I'm now a certified software developer. My stack includes React, Next.js, TypeScript & JavaScript, Tailwind, Prisma, MongoDB and Oracle.",
+    icon: React.createElement(FaReact),
+    date: "2023 - present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "NeatSeat",
+    description:
+      "NeatSeat is a workplace management software designed to streamline and optimize workspace allocation, providing efficient solutions for desk assignments and organizational seating arrangements.",
+    tags: ["React", "Node.js", "MDB", "Spring", "MSSQL", "Prisma"],
+    imageUrl: corpcommentImg,
+  },
+  {
+    title: "Flexibble",
+    description:
+      "A full stack Dribble clone with all the necessary features of dribble from sharing and showcasing projects.",
+    tags: ["React", "Next.js", "Next Auth", "Tailwind", "GraphQL", "Grafbase"],
+    imageUrl: rmtdevImg,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: wordanalyticsImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "Java",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "PostgreSQL",
+  "Python",
+  "Spring",
+  "MSSQL",
+  "Oracle Database",
+] as const;
