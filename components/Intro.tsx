8ee19326897ab96f8608d7dad323a65341159725
DrawingNea <lcy.li@outlook.de>
2024-01-01 14:35:16 +0100

    Add Intro text

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,25 +1,51 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const Intro = () => {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
-          <Image
-            src="https://media.licdn.com/dms/image/D4E03AQFMiLAhA0ERGQ/profile-displayphoto-shrink_200_200/0/1700738400333?e=1709769600&v=beta&t=7LXFwISllsk0VHC1sa_0pKj8lrae8Y5ip-ECkAwByIA"
-            alt="Evelyn portrait"
-            width={192}
-            height={192}
-            quality={95}
-            priority={true}
-            className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
-          />
-          <span className="absolute bottom-0 right-0 text-4xl">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src="https://media.licdn.com/dms/image/D4E03AQFMiLAhA0ERGQ/profile-displayphoto-shrink_200_200/0/1700738400333?e=1709769600&v=beta&t=7LXFwISllsk0VHC1sa_0pKj8lrae8Y5ip-ECkAwByIA"
+              alt="Evelyn portrait"
+              width={192}
+              height={192}
+              quality={95}
+              priority={true}
+              className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+            />
+          </motion.div>
+
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
             👋
-          </span>
+          </motion.span>
         </div>
       </div>
+      <p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl">
+        <span className="font-bold">Hello, I'm Evelyn.</span> I'm an{" "}
+        <span className="font-bold">experienced full-stack developer.</span> I
+        enjoy building <span className="italic">sites & apps</span>. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </p>
     </section>
   );
 };
