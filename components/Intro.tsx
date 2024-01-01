fad3bfa9784792a33f2dc953cb854843b4da775c
DrawingNea <lcy.li@outlook.de>
2024-01-01 14:16:45 +0100

    fix position of waving hand

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -5,7 +5,7 @@ const Intro = () => {
   return (
     <section>
       <div className="flex items-center justify-center">
-        <div>
+        <div className="relative">
           <Image
             src="https://media.licdn.com/dms/image/D4E03AQFMiLAhA0ERGQ/profile-displayphoto-shrink_200_200/0/1700738400333?e=1709769600&v=beta&t=7LXFwISllsk0VHC1sa_0pKj8lrae8Y5ip-ECkAwByIA"
             alt="Evelyn portrait"
@@ -15,7 +15,7 @@ const Intro = () => {
             priority={true}
             className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
           />
-          <span className="text-4xl">
+          <span className="absolute bottom-0 right-0 text-4xl">
             👋
           </span>
         </div>
