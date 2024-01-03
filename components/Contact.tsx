ae0f4dcda290741a84b3ac6b181ef0ee92808a29
DrawingNea <lcy.li@outlook.de>
2024-01-03 14:05:42 +0100

    Add animation to contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,12 +1,29 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./Section-Heading";
 import { FaPaperPlane } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Contact = () => {
+
   return (
-    <section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)]">
+    <motion.section
+      id="contact"
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{ opacity: 0 }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
+    >
       <SectionHeading>Contact me</SectionHeading>
-      <p className="text-gray-700">
+      <p className="text-gray-700 -mt-6">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:EveLcy@outlook.de">
           EveLcy@outlook.de
@@ -14,7 +31,11 @@ const Contact = () => {
         or through this form.
       </p>
       <form className="mt-10 flex flex-col" action="">
-        <input type="email" className="h-14 px-4 rounded-lg borderBlack" placeholder="Your email" />
+        <input
+          type="email"
+          className="h-14 px-4 rounded-lg borderBlack"
+          placeholder="Your email"
+        />
         <textarea
           className="h-52 my-3 rounded-lg borderBlack p-4"
           placeholder="Your message"
@@ -29,7 +50,7 @@ const Contact = () => {
           <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
         </button>
       </form>
-    </section>
+    </motion.section>
   );
 };
 
