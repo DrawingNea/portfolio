1f58cf40f520ee8d1e9fb053cfc47e8ad9017a1e
DrawingNea <lcy.li@outlook.de>
2024-01-05 22:57:24 +0100

    Add time of last click to projects component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,13 +9,13 @@ import { useActiveSectionContext } from "@/context/Active-Section-Context";
 
 const Projects = () => {
   const { ref, inView } = useInView({ threshold: 0.5 });
-  const { setActiveSection } = useActiveSectionContext();
+  const { setActiveSection, timeOfLastClick} = useActiveSectionContext();
 
   useEffect(() => {
-    if (inView) {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
       setActiveSection("Projects");
     }
-  }, [inView, setActiveSection]);
+  }, [inView, setActiveSection, timeOfLastClick]);
   return (
     <section id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My Projects</SectionHeading>
