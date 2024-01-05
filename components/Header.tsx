f1433ef0d698f664c7abd34abe74b7141ad55ff2
DrawingNea <lcy.li@outlook.de>
2024-01-05 22:56:33 +0100

    Update time of last click on Header Component

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ import clsx from "clsx";
 import ActiveSectionContext, { useActiveSectionContext } from "@/context/Active-Section-Context";
 
 const Header = () => {
-  const {activeSection, setActiveSection} = useActiveSectionContext();
+  const {activeSection, setActiveSection, setTimeOfLastClick} = useActiveSectionContext();
   return (
     <header className="z-[999] relative">
       <motion.div
@@ -33,7 +33,10 @@ const Header = () => {
                   "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition",
                   { "text-gray-950": activeSection === link.name }
                 )}
-                onClick={() => setActiveSection(link.name)}
+                onClick={() => {
+                  setActiveSection(link.name)
+                  setTimeOfLastClick(Date.now())
+                }}
               >
                 {link.name}
                 {link.name === activeSection && (
