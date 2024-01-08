a97c85f1f7788968b1b12f8fc0c0d81209c8d0b3
DrawingNea <lcy.li@outlook.de>
2024-01-09 00:02:06 +0100

    Add base footer

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <small>
+            &copy; 2024 Evelyn Lindner. All rights reserved.
+        </small>
+        <p>
+            <span>About this website:</span> built with React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel hosting.
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
