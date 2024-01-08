b829e9b21a893c8a64a341af934677895c269cb1
DrawingNea <lcy.li@outlook.de>
2024-01-09 00:21:34 +0100

    Include prefered color scheme of system

--- components/Theme-Switch.tsx
+++ components/Theme-Switch.tsx
@@ -8,18 +8,20 @@ const ThemeSwitch = () => {
   const toggleTheme = () => {
     if (theme === "light") {
       setTheme("dark");
-      window.localStorage.setItem("theme","dark");
+      window.localStorage.setItem("theme", "dark");
     } else {
       setTheme("light");
-      window.localStorage.setItem("theme","light");
+      window.localStorage.setItem("theme", "light");
     }
   };
-  useEffect(()=> {
-   const localTheme = window.localStorage.getItem("theme") as Theme | null;
-    if(localTheme){
-        setTheme(localTheme);
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme") as Theme | null;
+    if (localTheme) {
+      setTheme(localTheme);
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
     }
-  },[])
+  }, []);
   return (
     <button
       className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all"
