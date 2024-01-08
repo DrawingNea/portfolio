cdcad1f21888ea4519bc0b9fb18832066b3d267d
DrawingNea <lcy.li@outlook.de>
2024-01-08 17:22:29 +0100

    Add loading spinner while submitting form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,10 +2,10 @@
 
 import React from "react";
 import SectionHeading from "./Section-Heading";
-import { FaPaperPlane } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 import { sendEmail } from "@/actions/email";
+import SubmitBtn from "./Submit-Btn";
 
 const Contact = () => {
   const { ref } = useSectionInView("Contact");
@@ -55,13 +55,7 @@ const Contact = () => {
           required
           maxLength={5000}
         />
-        <button
-          type="submit"
-          className="group flex items-center justify-center h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
-        >
-          Submit{" "}
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   );
