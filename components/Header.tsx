08759bb737061dc498cc5c40eba3affc445b2a70
DrawingNea <lcy.li@outlook.de>
2024-01-08 19:08:48 +0100

    Make Navbar width smaller because of removed contact link

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
       <motion.div
         className="flex fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border 
       border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] 
-      backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full"
+      backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[31rem] sm:rounded-full"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
