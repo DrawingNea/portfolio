f0e92fcfff3f84138c0d8b5d41c55acc3e6b3b73
DrawingNea <lcy.li@outlook.de>
2024-01-09 00:33:54 +0100

    Finish dark mode for Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
       <motion.div
         className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border 
       border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] 
-      backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[31rem] sm:rounded-full"
+      backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[31rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
@@ -30,8 +30,8 @@ const Header = () => {
               <Link
                 href={link.hash}
                 className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition",
-                  { "text-gray-950": activeSection === link.name }
+                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
+                  { "text-gray-950 dark:text-gray-200": activeSection === link.name }
                 )}
                 onClick={() => {
                   setActiveSection(link.name)
@@ -40,7 +40,7 @@ const Header = () => {
               >
                 {link.name}
                 {link.name === activeSection && (
-                  <motion.span className="bg-gray-100 rounded-full absolute inset-0 -z-10" layoutId="activeSection" transition={{type:"spring",stiffness:380,damping:30,}}></motion.span>
+                  <motion.span className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800" layoutId="activeSection" transition={{type:"spring",stiffness:380,damping:30,}}></motion.span>
                 )}
               </Link>
             </motion.li>
