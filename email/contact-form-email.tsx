22eed87dd6b495af2bcc6379ae18820b4583d381
DrawingNea <lcy.li@outlook.de>
2024-01-08 17:01:42 +0100

    Style email content

--- email/contact-form-email.tsx
+++ email/contact-form-email.tsx
@@ -1,31 +1,46 @@
-import React from 'react'
-import { Html, Body, Head, Heading, Hr, Container, Preview, Section, Text } from "@react-email/components"
+import React from "react";
+import {
+  Html,
+  Body,
+  Head,
+  Heading,
+  Hr,
+  Container,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
 import { Tailwind } from "@react-email/tailwind";
 
 type ContactFormEmailProps = {
-    senderMessage: string;
-    senderEmail: string;
-}
+  senderMessage: string;
+  senderEmail: string;
+};
 
-const ContactFormEmail = ({senderMessage,senderEmail}:ContactFormEmailProps) => {
+const ContactFormEmail = ({
+  senderMessage,
+  senderEmail,
+}: ContactFormEmailProps) => {
   return (
     <Html>
-        <Head />
-        <Preview>New message from your portfolio site</Preview>
-        <Tailwind>
-            <Body>
-                <Container>
-                    <Section>
-                        <Heading>You received the following message from the contact form</Heading>
-                        <Text>{senderMessage}</Text>
-                        <Hr />
-                        <Text>The sender's email is: {senderEmail}</Text>
-                    </Section>
-                </Container>
-            </Body>
-        </Tailwind>
+      <Head />
+      <Preview>New message from your portfolio site</Preview>
+      <Tailwind>
+        <Body className="bg-gray-100 text-black">
+          <Container>
+            <Section className="bg-white borderBlack my-10 px-10 py-4 rounded-md">
+              <Heading className="leading-tight">
+                You received the following message from the contact form
+              </Heading>
+              <Text>{senderMessage}</Text>
+              <Hr />
+              <Text>The sender's email is: {senderEmail}</Text>
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
-  )
-}
+  );
+};
 
-export default ContactFormEmail
+export default ContactFormEmail;
