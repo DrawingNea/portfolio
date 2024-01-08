66fb5e5b6512f0b0defbeced54fda17353aae9f9
DrawingNea <lcy.li@outlook.de>
2024-01-09 00:13:02 +0100

    Add toggle function to theme switch

--- components/Theme-Switch.tsx
+++ components/Theme-Switch.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BsSun } from 'react-icons/bs'
 
+type Theme = "light" | "dark";
+
 const ThemeSwitch = () => {
+    const [theme, setTheme] = useState<Theme>("light");
+    const toggleTheme = () => {
+        if( theme === "light" ) {
+            setTheme("dark");
+        }else{
+            setTheme("light");
+        }
+    }
   return (
-    <button className='fixed bottom-5 right-5'>
+    <button className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all'
+    onClick={toggleTheme}
+    >
         <BsSun />
     </button>
   )
