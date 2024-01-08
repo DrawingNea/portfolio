3e36b886cdaf331c5bf931e31a6b4143ebfa4d50
DrawingNea <lcy.li@outlook.de>
2024-01-08 17:37:19 +0100

    Fix email to address

--- actions/email.tsx
+++ actions/email.tsx
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "Lcy.Li@outlook.de",
+      to: "lcy.li@outlook.de",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
       react: React.createElement(ContactFormEmail, {
