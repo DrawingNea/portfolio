82ebd66f0cffd9553a195d7943dd34928f6c4101
DrawingNea <lcy.li@outlook.de>
2024-01-08 18:41:05 +0100

    Change config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "media.licdn.com"
-            }
-        ]
-    },
-    output: 'export',
-}
+  output: "export",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
